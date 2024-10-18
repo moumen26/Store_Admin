0a38b3a8a03016d68e4fe61a214db6de7547e93b
ZINE EDDINE <119061162+ZYNOCODES@users.noreply.github.com>
2024-10-18 15:33:48 +0100

    fetching clients API integration commit

--- src/pages/Shops.jsx
+++ src/pages/Shops.jsx
@@ -4,14 +4,60 @@ import Search from "../components/Search";
 import ButtonAdd from "../components/ButtonAdd";
 import ShopsTable from "../components/ShopsTable";
 import ButtonExportExcel from "../components/ButtonExportExcel";
+import { useQuery } from "@tanstack/react-query";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { TokenDecoder } from "../util/DecodeToken";
+import { useLocation } from "react-router-dom";
 
 export default function Shops() {
+  const { user } = useAuthContext();
+  const decodedToken = TokenDecoder();
+  const location = useLocation();
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredData, setFilteredData] = useState([]);
 
   const handleSearchChange = (e) => {
     setSearchQuery(e.target.value);
   };
+
+  //---------------------------------API calls---------------------------------\\
+
+  //fetch data
+  const fetchClientData = async () => {
+    const response = await fetch(
+      `${import.meta.env.VITE_APP_URL_BASE}/Client/admin/unverified/${decodedToken.id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user?.token}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      if (errorData.error.statusCode === 404) {
+        return []; // Return an empty array if no data is found
+      } else {
+        throw new Error("Error receiving Client data");
+      }
+    }
+
+    return await response.json(); // Return the data if the response is successful
+  };
+  // useQuery hook to fetch data
+  const {
+    data: ClientData,
+    error: ClientDataError,
+    isLoading: ClientDataLoading,
+    refetch: refetchClientData,
+  } = useQuery({
+    queryKey: ["ClientData", user?.token, location.key],
+    queryFn: fetchClientData,
+    enabled: !!user?.token, // Ensure the query runs only if the user is authenticated
+    refetchOnWindowFocus: true, // Optional: refetch on window focus
+  });
 
   return (
     <div className="pagesContainer">
@@ -32,6 +78,8 @@ export default function Shops() {
           <ShopsTable
             searchQuery={searchQuery}
             setFilteredData={setFilteredData}
+            data={ClientData}
+            loading={ClientDataLoading}
           />
         </div>
       </div>
